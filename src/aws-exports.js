7ea262feaadc43bc6e36dee5d359198ad3cf9626
Jongkyu-Hong <jk7796.hong@samsung.com>
2021-08-07 20:53:32 +0900

    Add graphQL

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -21,7 +21,11 @@ const awsmobile = {
     "aws_cognito_password_protection_settings": {
         "passwordPolicyMinLength": 8,
         "passwordPolicyCharacters": []
-    }
+    },
+    "aws_appsync_graphqlEndpoint": "https://kdzs6tkkzrguhakgcqmz6gptfu.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-wjombgarzzf2flbjtmau3yl7sa"
 };
 
 
